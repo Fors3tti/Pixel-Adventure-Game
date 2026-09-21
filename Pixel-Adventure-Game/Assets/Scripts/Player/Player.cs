4d6b85696e1c86a8ba7c38734c1f9aebbeed3ca9
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2026-09-21 12:53:15 -0400

    Added Jump and Collision Detections

--- Pixel-Adventure-Game/Assets/Scripts/Player/Player.cs
+++ Pixel-Adventure-Game/Assets/Scripts/Player/Player.cs
@@ -5,7 +5,14 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Collision info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
 
     private float xInput;
 
@@ -17,10 +24,30 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        HandleCollision();
+        HandleInput();
+        HandleMovement();
+        HandleAnimations();
+    }
+
+    private void HandleInput()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
 
-        HandleAnimations();
-        HandleMovement();
+        if (Input.GetKeyDown(KeyCode.X) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    }
+
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
     }
 
     private void HandleAnimations()
@@ -31,5 +58,11 @@ public class Player : MonoBehaviour
     private void HandleMovement()
     {
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(
+            transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
