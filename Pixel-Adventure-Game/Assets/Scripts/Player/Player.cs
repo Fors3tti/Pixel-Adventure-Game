63add792b6641b176adaa7da1cf0f5fec33bd225
fors3tti <152114883+Fors3tti@users.noreply.github.com>
2026-09-21 20:30:22 -0400

    Added Double Jump Mechanics

--- Pixel-Adventure-Game/Assets/Scripts/Player/Player.cs
+++ Pixel-Adventure-Game/Assets/Scripts/Player/Player.cs
@@ -5,16 +5,23 @@ public class Player : MonoBehaviour
     private Rigidbody2D rb;
     private Animator anim;
 
-    [Header("Movement")]
+    [Header("Movement info")]
     [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float doubleJumpForce;
+
+    public bool canDoubleJump;
 
     [Header("Collision info")]
     [SerializeField] private float groundCheckDistance;
     [SerializeField] private LayerMask whatIsGround;
     private bool isGrounded;
+    private bool isAirborne;
 
     private float xInput;
+
+    private bool facingRight = true;
+    private int facingDir = 1;
 
     private void Awake()
     {
@@ -24,23 +31,61 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        UpdateAirbornStatus();
+
         HandleCollision();
         HandleInput();
         HandleMovement();
+        HandleFlip();
         HandleAnimations();
+    }
+
+    private void UpdateAirbornStatus()
+    {
+        if (isGrounded && isAirborne)
+            HandleLanding();
+
+        if (!isGrounded && !isAirborne)
+            BecomeAirborne();
+    }
+
+    private void BecomeAirborne()
+    {
+        isAirborne = true;
+    }
+
+    private void HandleLanding()
+    {
+        isAirborne = false;
+        canDoubleJump = true;
     }
 
     private void HandleInput()
     {
         xInput = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetKeyDown(KeyCode.X) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.X))
+            JumpButton();
+    }
+
+    private void JumpButton()
+    {
+        if (isGrounded)
         {
             Jump();
+        }
+        else if (canDoubleJump)
+        {
+            DoubleJump();
         }
     }
 
     private void Jump() => rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+    private void DoubleJump()
+    {
+        canDoubleJump = false;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, doubleJumpForce);
+    }
 
     private void HandleCollision()
     {
@@ -57,6 +102,21 @@ public class Player : MonoBehaviour
     private void HandleMovement()
     {
         rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+    }
+
+    private void HandleFlip()
+    {
+        if (rb.linearVelocity.x < 0 && facingRight || rb.linearVelocity.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingDir = facingDir * -1;
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
     }
 
     private void OnDrawGizmos()
